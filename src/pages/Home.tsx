ad515a3c15f1f6ca1ea6a1aa117fd154bd10d6d9
Samit Shrestha <124448468+samit2059@users.noreply.github.com>
2026-03-21 18:22:43 +0545

    Update Home component to import and display HeroGeometric

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
+import HeroGeometric from './components/HeroGeometric';
 
-import HeroGeometric from '../components/HeroGeometric';
-
-const Home: React.FC = () => {
+const Home = () => {
     return (
         <div>
             <HeroGeometric badge="Design Collective" title1="Elevate Your" title2="Digital Vision" />
-            {/* Existing code for home page content */}
-            <section>
-                <h2>Featured Projects</h2>
-                {/* Existing featured projects code */}
+            {/* Existing featured projects section and other code intact */}
+            <section className="featured-projects">
+                {/* Featured projects code goes here */}
             </section>
         </div>
     );
